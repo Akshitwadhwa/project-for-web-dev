2dc6b38cb64ab087dbe6c7093923ccb209330aaa
Akshitwadhwa <akshitwadhwa06@gmail.com>
2024-12-01 23:26:28 +0530

    nodejs linking

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path =  require("path");
+const app = express();
+
+//console.log(__dirname);
+
+//console.log(path.join(__dirname,"../public"));
+const staticPath =  path.join(__dirname, "../public");
+
+// this will let us use the 
+app.use(express.static(staticPath));
+
+app.get("/about" , (req ,res) => {
+    return res.send("you are at the webiste");
+});
+
+app.get("/name" , (req ,res) => {
+    return res.send('hi');
+});
+
+app.listen(5001, () => {
+    console.log("lisiting the port at 5001");
+
+});
